b8c242a8b5604329642b07bf195f685744935c82
Pierre PAUL <pierre.paul13pp@gmail.com>
2024-04-22 09:54:24 +0200

    Update Pierron-33183.ino

--- src/Pierron-33183/Pierron-33183.ino
+++ src/Pierron-33183/Pierron-33183.ino
@@ -1,15 +1,11 @@
-#include <Arduino.h>
-#include <SoftwareSerial.h>
-
 void setup() {
   pinMode(A0,INPUT);
   Serial.begin(115200);
 }
 
 void loop() {
-  float Luminosite;
-  Luminosite = analogRead(A0+0);
+  double Luminosite = analogRead(A0);
   Luminosite = 100 * (Luminosite / 1023);
-  Serial.println("Luminosite : " + String(Luminosite) + "%");
-  delay(1000);
+  Serial.println("Luminosite : " + String(Luminosite, 0) + "%");
+  delay(500);
 }
